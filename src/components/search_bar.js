4fe4625b4f21172ca3a1b7edaa8f48e01fa994fc
Sharky <kayden.althen@gmail.com>
2017-10-29 14:26:51 -0700

    Progress: 3:34

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+// only class-based components have state, functional components do not
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        // inside the constructor function is the only place where state
+        // will be explicitly set, with =. don't do it anywhere else like this
+        this.state = { term: '' };
+    }
+
+    // whenever state changes, the render function will be called
+    render() {
+        // onChange is a specific React-defined event handler
+        return (
+            <div className="search-bar">
+                <input
+                    value={this.state.term} // the state is telling the input what its value is
+                    onChange={e => this.onInputChange(e.target.value)}
+                />
+            </div>
+        );
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBFTd2-fZcBE2RM1QLC3yU15weIJAn4m68';
 
+
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, videos => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it in the DOM
-// Make an instance of the class by wrapping it the class in tags </>
+// Make an instance of the class by wrapping the class in tags </>
 ReactDOM.render(<App/>, document.querySelector('.container'));
